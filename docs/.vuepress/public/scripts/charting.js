de6643aa85a679471717ba962ea6bee7db511078
FriendlyUser <pandabeareee@gmail.com>
2018-08-01 21:02:43 -0700

    proper deployment!

--- docs/.vuepress/public/scripts/charting.js
+++ docs/.vuepress/public/scripts/charting.js
@@ -1,12 +1,10 @@
-<script>
-  $(function(){
-        var reg = /^[\'\"]+|[\'\"]+$/g;
-         //-  chart.js
-        var chartjsLen = $('.chartjs').length;
-        var myChartJs = [];
-        for(var i = 0;i < chartjsLen; i++){
-            var ctx = $('.chartjs')[i].getContext('2d');
-            myChartJs[i] = new Chart(ctx,$.parseJSON($($('.chartjs')[i])[0].innerHTML))
-        }
-  })
-</script>
+$(function(){
+    var reg = /^[\'\"]+|[\'\"]+$/g;
+     //-  chart.js
+    var chartjsLen = $('.chartjs').length;
+    var myChartJs = [];
+    for(var i = 0;i < chartjsLen; i++){
+        var ctx = $('.chartjs')[i].getContext('2d');
+        myChartJs[i] = new Chart(ctx,$.parseJSON($($('.chartjs')[i])[0].innerHTML))
+    }
+ })
